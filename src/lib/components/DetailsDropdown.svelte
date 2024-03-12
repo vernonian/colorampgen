a20cf0c32bbb9a7f58215ddb838cfdaa9853cf71
vernonian <121730895+vernonian@users.noreply.github.com>
2024-03-12 13:02:30 -0700

    CSS cleanup, removed unused styles

--- src/lib/components/DetailsDropdown.svelte
+++ src/lib/components/DetailsDropdown.svelte
@@ -3,17 +3,33 @@
 
 </script>
 
-<details>
+<details class="body-s">
   <summary>{summary}</summary>
-  <div class="details-inner body-s">
+  <div class="details-inner">
     <slot></slot>
   </div>
 </details>
 
 <style>
+
+  details {
+    color: var(--gray-8);
+  }
+
+
+  summary {
+    padding: var(--xs);
+    cursor: pointer;
+    transition: var(--transition-m);
+  }
+
+  summary:hover {
+    background-color: var(--gray-2);
+  }
+
   .details-inner {
     padding: var(--xs) var(--s);
-    background-color: var(--gray-3);
+    background-color: var(--gray-2);
     display: flex;
     flex-direction: column;
     gap: var(--s);
