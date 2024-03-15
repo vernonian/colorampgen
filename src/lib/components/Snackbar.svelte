c5a8aea2db30ee2dda024f7bfa9d297f721495a4
vernonian <121730895+vernonian@users.noreply.github.com>
2024-03-15 12:36:56 -0700

    Added transition to snackbar

--- src/lib/components/Snackbar.svelte
+++ src/lib/components/Snackbar.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition';
+
   // Props
   export let message:string = "Default Text";
   export let duration:number = 2000; // milliseconds
 
   let isVisible:boolean = false;
+  let classes:string;
 
   export function showSnackbar() {
     isVisible = true;
@@ -15,7 +18,7 @@
 
 <!-- Show the snackbar when the isVisible prop is true -->
 {#if isVisible}
-  <div class="snackbar">
+  <div class="snackbar" transition:fade={{ duration: 200 }}>
     <p>{message}</p>
   </div>
 {/if}
@@ -25,12 +28,12 @@
     padding: var(--padding-inline);
     background-color: var(--gray-9);
     color: var(--white);
-    box-shadow: 0px var(--xs) var(--s) 0px rgba(0,0,0,0.5);
+    box-shadow: 0px var(--xs) var(--s) 0px rgba(0,0,0,0.25);
     position: fixed;
     top: var(--m);
     left: 50%;
     font-size: var(--fontsizebody-s);
-    transform: translateX(-50%);
+    transform: translateX(-50%) translateY(1rem);
     z-index: var(--z-max);
     transition: var(--transition-m);
   }
